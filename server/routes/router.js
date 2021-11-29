775d83cbe1d223d93efc5328c3afcc387f7dd2ef
BhargavSushant <bhargavsushant@gmail.com>
2021-11-29 17:07:22 -0500

    before creating db

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,23 +3,40 @@ const route = express.Router();
 // instead of const app = express() we do this because express() will create a new app
 // we want only router
 
-route.get("/", (req, res) => {
-  // this method will allow us to render html file
-  // res.send("CRUD App");
-  res.render("index");
-});
+const services = require("../services/render");
+// route.get("/", (req, res) => {
+//   // this method will allow us to render html file
+//   // res.send("CRUD App");
+//   // res.render("index");
+// });
 
-route.get("/add-user", (req, res) => {
-  // this method will allow us to render html file
-  // res.send("CRUD App");
-  res.render("add_user");
-});
+/**
+ * @description Root Route
+ * @method GET /
+ */
+route.get("/", services.homeRoutes);
 
-route.get("/update-user", (req, res) => {
-  // this method will allow us to render html file
-  // res.send("CRUD App");
-  res.render("update_user");
-});
+// route.get("/add-user", (req, res) => {
+//   // this method will allow us to render html file
+//   // res.send("CRUD App");
+//   res.render("add_user");
+// });
+/**
+ * @description Add User Route
+ * @method GET /add-user
+ */
+route.get("/add-user", services.add_user);
+
+// route.get("/update-user", (req, res) => {
+//   // this method will allow us to render html file
+//   // res.send("CRUD App");
+//   res.render("update_user");
+// });
+/**
+ * @description Update User Route
+ * @method GET /update-user
+ */
+route.get("/update-user", services.update_user);
 
 // deploy middleware
 module.exports = route;
