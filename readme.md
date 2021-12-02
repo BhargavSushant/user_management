4a88deac8c2522a56828b9ec0494d2698be4fe90
Bhargav Sushant <BhargavSushant@gmail.com>
2021-12-03 03:44:46 +0530

    added readme

---
+++ readme.md
@@ -0,0 +1,12 @@
+# 1. INTRODUCTION
+
+This project is a simple demonstration of authentication , authorization using Mongoose library.
+DBMS : MONGODB , NODE JS , EXPRESS FOR CONTROL & MODEL,EJS FOR VIEW ARE USED.
+Authorized user can login, logout, add , delete , update products, add user 
+
+
+# 2. PROJECT STRUCTURE
+
+# 3. REFERENCES
+
+## 3.1 DEPRACTATION https://mongoosejs.com/docs/deprecations.html#-findandmodify-
--- server/controller/controller.js
+++ server/controller/controller.js
@@ -37,10 +37,34 @@ exports.create = async (req, res) => {
 };
 
 // retrieve & return all user(s)
-exports.find = (req, res) => {};
+exports.find = (req, res) => {
+  Userdb.find()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message || "ERROR IN RETRIEVE" });
+    });
+};
 
 //Update a new identified user by userid
-exports.update = (req, res) => {};
+// Logic = if request body is not available then throw 400 err
+//          else find by id and update
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Input data can not be empty" });
+  }
+  const id = req.params.id; // in express url parameters and query parameters are there
+  // we can use them using params keyword , this here is url params
+  /** useFindAndModify is depracted so we have to set it false here see ref#1 in readme.md*/
+  Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false }).then(
+    (data) => {
+      if (!data) {
+        res.status(404).send({ message: `Cant update user with id ${id}` });
+      }
+    }
+  );
+};
 
 //Delete a user wrt userid
 exports.delete = (req, res) => {};
